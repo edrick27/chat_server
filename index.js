6f9119407ad868e2ccbffb0766ac43a30ad08251
edrick27 <edricklpcr@gmail.com>
2021-12-08 10:37:05 -0600

    fix error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const cors = require('cors');
-// const fs = require('fs');
+const fs = require('fs');
 
 
 // DB Config
